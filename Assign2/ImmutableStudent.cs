c940ddb580fed86a00b59d7809bf7d08bb92c829
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-09-20 14:33:44 +0200

    Implement ImmutiableStudent

---
+++ Assign2/ImmutableStudent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assign2
+{
+	public record ImmutableStudent(
+		int Id,
+		string GivenName,
+		string Surname
+		)
+	{
+		public Student.StudentStatus Status
+		{
+			get
+			{
+				DateTime now = DateTime.Now;
+				if (StartDate <= now && now <= EndDate)
+					return Student.StudentStatus.Active;
+				if (EndDate < now)
+					if (EndDate >= GraduationDate)
+						return Student.StudentStatus.Graduated;
+					else
+						return Student.StudentStatus.Dropout;
+				return Student.StudentStatus.New;
+			}
+		}
+		public DateTime StartDate { get; init; }
+		public DateTime EndDate { get; init; }
+		public DateTime GraduationDate { get; init; }
+	}
+}
